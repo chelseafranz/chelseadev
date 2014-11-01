7f3be923bb39c958be1d64ca99782e3383aecdb5
Chelsea Franz <chelseafranz3@gmail.com>
2014-11-01 17:51:22 -0400

    basic layout, navigate withing poage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,30 @@
-// $('.topmenu').hover( function () {
-// 	$('.topmenu').animate({marginRight: '-20px' }, 1000, 'linear');
-// });
+$('.topmenu').on('click', function () {
+	$('.topmenu').animate({right: '0px' }, 1000, 'linear');
+	$('.topmenu').addClass('see');
+});
 
 // $('.hero-unit').hover( function () {
 // 	$('.topmenu').animate({marginRight: '-160px' }, 1000, 'linear');
 // });
 
+var Portfolio= Backbone.Model.extend({
+	defaults:{
+		name: '',
+		description:'', 
+	},
 
+	initialize: function(){
+		var a= this.get('name');
+		console.log(a + ' has been added to your portfolio.');
+	}
+});
 
+var PortfolioCollection= Backbone.Collection.extend({
+	model: Portfolio,
+	url:'http://tiy-atl-fe-server.herokuapp.com/collections/chelsea7'
+});
 
+var all_items= new PortfolioCollection();
 
 
 
