d7918200da3224b39c8ededfc2d18e4273bdbc03
Chelsea Franz <chelseafranz3@gmail.com>
2014-11-02 17:54:00 -0500

    portfolio section basic layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,12 +19,71 @@ var Portfolio= Backbone.Model.extend({
 	}
 });
 
-var PortfolioCollection= Backbone.Collection.extend({
+var Items= Backbone.Collection.extend({
 	model: Portfolio,
 	url:'http://tiy-atl-fe-server.herokuapp.com/collections/chelsea7'
 });
 
-var all_items= new PortfolioCollection();
+var all_items= new Items();
+
+$('#form').on('submit', function(a){
+a.preventDefault();
+
+var task_name=$('#itemName').val();
+
+var t= new Portfolio({
+	name: task_name
+});
+
+all_items.add(t);
+t.save();
+
+$(this)[0].reset();
+
+});
+
+// portfolio section/////////////////////////////
+var portfolio = {
+  projects : [
+    { name: 'Surf and Paddle', date: 'September 23, 2014', link: 'https://github.com/chelseafranz/surf-and-paddle' },
+    { name: 'Accordion', date: '', link: 'https://chelseafranz.github.com/accord'},
+    { name: 'GitHub Profile', date: '', link: 'http://chelseafranz.github.com/gitprofile'},
+    { name: 'Etsy Page', date: '', link: 'https://chelseafranz.github.com/etsy'},
+    { name: 'Reasturant Website', date: '', link: 'https://chelseafranz.github.com/spiked'},
+    { name: 'To-Do List', date: '', link: 'http://chelseafranz.github.com/todolist'},
+    { name: 'Turn-Based Game', date: '', link: 'http://chelseafranz.github.com/game'}
+  ]
+};
+
+var rendered = Handlebars.templates['portfolio'];
+$('#portfolio').append(rendered(portfolio));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ app/scripts/templates.js
@@ -0,0 +1,16 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['portfolio'] = template({"1":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<li class=\"projectName\">\n	"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "\n	<div>\n	<a href='"
+    + escapeExpression(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"link","hash":{},"data":data}) : helper)))
+    + "'>\n		view on github\n	</a>\n	</div>\n</li>\n\n\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1;
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.projects : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { return stack1; }
+  else { return ''; }
+  },"useData":true});
+})();
